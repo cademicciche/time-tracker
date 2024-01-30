53ccb1727fa5f842ca6fb5aceeee2712c69969b7
Cade M <cademicciche@gmail.com>
2024-01-30 18:12:12 -0500

    add chakra-ui kit

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function Hello() {
   return (
@@ -41,10 +42,12 @@ function Hello() {
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Hello />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
 }
